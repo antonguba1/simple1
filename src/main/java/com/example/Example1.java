6563da4b8a81151d0237976cd1b83d64fff9912c
Anton_Guba <Anton_Guba@epam.com>
2018-02-03 14:42:43 +0100

    add first class and test

---
+++ src/main/java/com/example/Example1.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Example1 {
+  public String concatenate(String one, String two){
+    return one + two;
+  }
+
+}
---
+++ src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+  @Test
+  public void testConcatenate() {
+    Example1 example = new Example1();
+
+    String result = example.concatenate("one", "two");
+
+    System.out.println("Verify result = onetwo");
+    assertEquals("onetwo", result);
+
+  }
+
+}
